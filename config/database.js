c8462370427aba684f44abd5c32127aa953cacfd
Frida Knoph <fridaknoph@gmail.com>
2022-11-04 15:36:06 +0100

    second fix

--- config/database.js
+++ config/database.js
@@ -19,8 +19,11 @@ module.exports = ({ env }) => {
             username: config.user,
             password: config.password
           },
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          },
           options: {
-            ssl: false
+            ssl: env.bool('DATABASE_SSL', false),
           }
         }
       }
