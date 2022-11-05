e53da9c377029c9fdfd39af129f574f0a7b6b107
Frida Knoph <fridaknoph@gmail.com>
2022-11-05 14:40:20 +0100

    update

--- config/database.js
+++ config/database.js
@@ -1,25 +1,6 @@
 const path = require('path');
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL)
 
 module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-            ssl: {
-            rejectUnauthorized: false, // For self-signed certificates
-          },
-        },
-          debug: false,
-          },
-        });
-
-  return {
   connection: {
     client: 'sqlite',
     connection: {
@@ -27,4 +8,4 @@ module.exports = ({ env }) => ({
     },
     useNullAsDefault: true,
   },
-};
+});
