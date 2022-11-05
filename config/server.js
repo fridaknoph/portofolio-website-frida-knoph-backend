677397bf1131ee29e4c0c6dc4063b7a88ec90e70
Frida Knoph <fridaknoph@gmail.com>
2022-11-05 14:55:47 +0100

    update

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', 'jdsjsjsjjsjsjjsjsjsjjsjshvsyghavsag'),
+    keys: env.array('APP_KEYS', 'CyGgzHW+dRM4jAcDf7Dbqg==','Z+7HYnJy1sqqmzGrQxW4Vw==','IUyhTe9QUKEqPSFOS0m9dg==','6KlXZczZsO0xHMm3uALIMg=='),
   },
 });
